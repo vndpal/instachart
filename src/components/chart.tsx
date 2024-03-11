d7f90a4055f4824893584d0af8c746437a01c8bd
vndpal <undervinod@gmail.com>
2024-03-11 21:45:12 +0530

    trying new libraries

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,71 +1,26 @@
 "use client"
 import React, { useState } from 'react'
-import { ResponsiveLine } from '@nivo/line'
 import TransportationData from '@/utility/types/transportationData'
+import data from '@/utility/data'
+import { LineChart, XAxis, Tooltip, CartesianGrid, Line } from 'recharts'
 
 type Props = { data: TransportationData }
 
 const chart = (props: Props) => {
-    const [visible, setVisible] = useState(false);
-    const handleClick = () => {
-        setVisible(true);
-    }
+
     return (
         <>
-            <div>sdfsdf</div>
-            <button onClick={handleClick}>click em</button>
-            <div>
-                {visible ?
-                    <ResponsiveLine
-                        data={props.data}
-                        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-                        xScale={{ type: 'point' }}
-                        yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
-                        yFormat=" >-.2f"
-                        axisTop={null}
-                        axisRight={null}
-                        axisBottom={{
-                            tickSize: 5,
-                            tickPadding: 5,
-                            tickRotation: 0,
-                            legend: 'transportation',
-                            legendOffset: 36,
-                            legendPosition: 'middle'
-                        }}
-                        pointSize={10}
-                        pointColor={{ theme: 'background' }}
-                        pointBorderWidth={2}
-                        pointBorderColor={{ from: 'serieColor' }}
-                        pointLabelYOffset={-12}
-                        useMesh={true}
-                        legends={[
-                            {
-                                anchor: 'bottom-right',
-                                direction: 'column',
-                                justify: false,
-                                translateX: 100,
-                                translateY: 0,
-                                itemsSpacing: 0,
-                                itemDirection: 'left-to-right',
-                                itemWidth: 80,
-                                itemHeight: 20,
-                                itemOpacity: 0.75,
-                                symbolSize: 12,
-                                symbolShape: 'circle',
-                                symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                                effects: [
-                                    {
-                                        on: 'hover',
-                                        style: {
-                                            itemBackground: 'rgba(0, 0, 0, .03)',
-                                            itemOpacity: 1
-                                        }
-                                    }
-                                ]
-                            }
-                        ]}
-                    />
-                    : ""}
+            <div className='h-screen'>
+
+                <LineChart width={400} height={400} data={data} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
+                    <XAxis dataKey="name" />
+                    <Tooltip />
+                    <CartesianGrid stroke="#f5f5f5" />
+                    <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+                    <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+                </LineChart>
+
+                {/* use react charts https://react-charts.tanstack.com/docs/installation */}
             </div>
         </>
     )
